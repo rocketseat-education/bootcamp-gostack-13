6ed630bf89510b10868419c1eeebce8caf673e39
Diego Fernandes <diego.schell.f@gmail.com>
2019-07-31 13:27:57 -0300

    Testes

---
+++ __tests__/components/TechList.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { render, fireEvent } from '@testing-library/react';
+
+import { addTech } from '~/store/modules/techs/actions';
+import TechList from '~/components/TechList';
+
+jest.mock('react-redux');
+
+describe('TechList component', () => {
+  it('should render tech list', () => {
+    useSelector.mockImplementation(cb => cb({
+      techs: ['Node.js', 'ReactJS']
+    }));
+
+    const { getByTestId, getByText } = render(<TechList />);
+
+    expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
+    expect(getByTestId('tech-list')).toContainElement(getByText('ReactJS'));
+  });
+
+  it('should be able to add new tech', () => {
+    const { getByTestId, getByLabelText } = render(<TechList />);
+
+    const dispatch = jest.fn();
+
+    useDispatch.mockReturnValue(dispatch);
+
+    fireEvent.change(getByLabelText('Tech'), { target: { value: 'Node.js' } });
+    fireEvent.submit(getByTestId('tech-form'));
+
+    expect(dispatch).toHaveBeenCalledWith(addTech('Node.js'));
+  });
+});
---
+++ src/components/TechList/index.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { addTech } from '../../store/modules/techs/actions';
+
+// import { Container } from './styles';
+
+export default function TechList() {
+  const [newTech, setNewTech] = useState('');
+
+  const dispatch = useDispatch();
+  const techs = useSelector(state => state.techs);
+
+  function handleAddTech() {
+    dispatch(addTech(newTech));
+
+    setNewTech('');
+  }
+
+  return (
+    <form data-testid="tech-form" onSubmit={handleAddTech}>
+      <ul data-testid="tech-list">
+        {techs.map(tech => <li key={tech}>{tech}</li>)}
+      </ul>
+
+      <label htmlFor="tech">Tech</label>
+      <input id="tech" value={newTech} onChange={e => setNewTech(e.target.value)} />
+
+      <button onClick={handleAddTech}>Adicionar</button>
+    </form>
+  );
+}
